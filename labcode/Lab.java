432986ddd12a358c2353aead2af653b5634d7487
polly <p.tunstall2327@student.leedsbeckett.ac.uk>
2025-10-23 17:21:25 +0100

    [CODE] added testing case work using Junit

---
+++ labcode/Lab.java
@@ -0,0 +1,16 @@
+package labcode;
+
+public class Lab
+{
+    public static int addNumber(int num1, int num2)
+        {
+            return num1+num2;
+        }
+
+    public static boolean evenNumber(int number)
+        {
+            return number%2 == 0;
+        }
+
+
+}
---
+++ labcode/TestDriver.java
@@ -0,0 +1,65 @@
+package labcode;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+
+public class TestDriver
+{
+    public static void main(String[] args)
+    {}
+    private final Lab lab = new Lab();
+    int number[] = {2,4,6,8,10};
+
+
+    @RepeatedTest(value = 5)
+    void testEvenNumberWithMultipleDataPoints(RepetitionInfo repetitionInfo)
+    {
+        int index = repetitionInfo.getCurrentRepetition()-1;
+        int indexElement = number[index];
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {2,4,6,8,10})
+    void testEvenNumberWithDataSourcePoints(int number)
+    {
+        assertTrue(lab.evenNumber(number));
+    }
+
+    @Test
+    void testEvenNumberWithPostiveValue()
+    {
+        assertTrue(Lab.evenNumber(4));
+    }
+
+    @Test
+    void testAddNumberWithPositiveValues()
+    {
+        //lab lab = new lab();
+
+        int actual = Lab.addNumber(10,20);
+        int expecting = 30;
+
+
+        assertEquals(expecting,actual);
+    }
+
+    @Test
+    void testAddNumberWithNegativeValues()
+
+    {
+        //lab lab = new lab();
+
+        int actual = Lab.addNumber(-10,-20);
+        int expecting = -30;
+
+
+        assertEquals(expecting,actual);
+    }
+
+}
