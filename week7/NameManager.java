161ffac9b8a77c6ec55beba818dea8369e4741e3
polly <p.tunstall2327@student.leedsbeckett.ac.uk>
2025-12-04 13:37:57 +0000

    [CODE] added and completed week 7 collections exercise

---
+++ week7/NameManager.java
@@ -0,0 +1,46 @@
+package week7;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class NameManager
+{
+
+    private List<String> names = new ArrayList<String>();
+
+    public void addName(String name)
+    {
+
+        names.add(name);
+    }
+
+    public void printNames()
+    {
+
+        System.out.println("\nNames List is as follows : \n--------------------------\n");
+
+        // iterate over list and print each name
+        for(String name : names)
+            System.out.println(name);
+    }
+
+    public void removeLongNames()
+    {
+
+        Iterator<String> iter = names.iterator();
+
+        // iterate over the list looking for long names
+        while (iter.hasNext() ) {
+            String name = iter.next();
+
+            if ( name.length() > 15 )
+                iter.remove();	// remove using only safe method (while iterating)
+        }
+    }
+
+    // Could also do this using streams, e.g.
+    // names = names.stream().filter((x) -> x.length() <= 15).collect(Collectors.toList());
+
+
+}
